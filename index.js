21644083b6778d3164a118538ba71f02e9c8754a
Andrew Chemes <andrey.chemes@gmail.com>
2022-08-09 17:00:03 +0300

    Merge branch 'backend' of https://github.com/AndrewCh555/MERN into backend

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 import express from 'express';
-import jwt from 'jsonwebtoken';
-import bcrypt from 'bcrypt'
 import mongoose from 'mongoose';
-import { validationResult } from 'express-validator';
+import multer from 'multer';
 
-import { registerValidation} from './validations/auth.js';
+import { registerValidation, loginValidation, postCreateValidation} from './validations.js';
 
-import UserModel from './models/User.js';
-import checkAuth from './utils/checkAuth.js';
+import {handleValidationErrors, checkAuth } from './utils/index.js';
+
+import { UserController, PostController } from './controllers/index.js';
 
 mongoose
 .connect('mongodb+srv://Andrew:Andrew@cluster0.i2fql.mongodb.net/blog?retryWrites=true&w=majority')
@@ -16,115 +15,35 @@ mongoose
 
 const app = express();
 
+const storage = multer.diskStorage({
+    destination: (_, __, cb) => {
+        cb(null, 'uploads');
+    },
+    filename: (_, file, cb) => {
+        cb(null, file.originalname);
+    },
+});
+
+const upload = multer({ storage });
+
 app.use(express.json());
+app.use('/uploads', express.static('uploads'));
 
-app.post('/auth/login', async (req,res) => {
-    try {
-        const user = await UserModel.findOne({ email: req.body.email});
+app.post('/auth/login', loginValidation, handleValidationErrors, UserController.login);
+app.post('/auth/register', registerValidation, handleValidationErrors, UserController.register);
+app.get('/auth/me', checkAuth, UserController.getMe);
 
-        if (!user) {
-            return res.status(404).json({
-                message: 'User not found',
-            });
-        }
-
-
-        const isValidPass = await bcrypt.compare(req.body.password, user._doc.passwordHash);
-
-        if(!isValidPass) {
-            return res.status(400).json({
-                message: 'Wrong login or password',
-            });
-        }
-        
-        const token = jwt.sign(
-            {
-                _id: user._id,
-            },
-            'secret123',
-            {
-                expiresIn: '30d',
-            },
-            );
-
-    const {passwordHash, ...userData} = user._doc;
-
+app.post('/upload/', upload.single('image'), (req, res) => {
     res.json({
-        ...userData,
-         token
+        url: `/uploads/${req.file.originalname}`,
     });
-
-    } catch (err) {
-        console.log(err);
-    res.status(500).json({
-        message: 'Authorization failed',
-    });
-    }
 });
 
-app.post('/auth/register', registerValidation, async (req,res) => {
-    try {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json(errors.array());
-    }
-
-    const password = req.body.password;
-    const salt = await bcrypt.genSalt(10);
-    const hash = await bcrypt.hash(password, salt);
-
-    const doc = new UserModel({
-        email: req.body.email,
-        fullName: req.body.fullName,
-        avatarUrl: req.body.avatarUrl,
-        passwordHash: hash,
-    });
-    const user = await doc.save();
-
-    const token = jwt.sign(
-    {
-        _id: user._id,
-    },
-    'secret123',
-    {
-        expiresIn: '30d',
-    },
-    );
-
-    const {passwordHash, ...userData} = user._doc;
-
-    res.json({
-        ...userData,
-         token
-    });
-} catch (err) {
-    console.log(err);
-    res.status(500).json({
-        message: 'Registration failed',
-    });
-}
-})
-
-app.get('/auth/me', checkAuth, async (req,res) => {
-try {
-    const user = await UserModel.findById(req.userId);
-
-    if (!user) {
-        return res.status(4044).json({
-            message: 'User not found',
-        })
-    }
-    
-    const {passwordHash, ...userData} = user._doc;
-
-    res.json(userData);
-} catch (err) {
-    console.log(err);
-    res.status(500).json({
-        message: 'Access denied',
-    });
-}
-});
+app.get('/posts', PostController.getAll);
+app.get('/posts/:id', PostController.getOne);
+app.post('/posts', checkAuth, postCreateValidation, handleValidationErrors, PostController.create);
+app.delete('/posts/:id', checkAuth, PostController.remove);
+app.patch('/posts/:id', checkAuth, postCreateValidation, handleValidationErrors, PostController.update);
 
 app.listen(4444, (err) => {
     if (err) {
@@ -132,3 +51,5 @@ app.listen(4444, (err) => {
     }
     console.log('Server OK');
 });
+
+// last version
